a4044e97b28f53bf2543e515e153ac11229e591c
unknown <francoparodi2001@gmail.com>
2023-08-30 13:53:28 -0300

    Agregando consignas

---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+# Crear un programa que le solicite al usuario un entero y lo imprima por pantalla. 
+# Recordá que podés usar las funciones input (para solicitar información) y print para mostrar
+# información.
