bf238634cc7dbdef5b0ae3da095e11dcb55794a5
unknown <francoparodi2001@gmail.com>
2023-08-30 13:54:27 -0300

    Agregando consignas

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,6 @@
+# Vamos con otro un poco más complejo. Para el siguiente programa a realizar, se pide hacer
+# dos variables que guarden dentro números, y luego sumarlos. El resultado se tendrá que
+# guardar en otra variable, y luego imprimir este resultado.
 tres = 3
 dos = 2
 siete = 7
